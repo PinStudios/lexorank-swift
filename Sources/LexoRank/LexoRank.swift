d0b2339501cc20cae59537be8e4215a8517850fa
Raimundas Sakalauskas <raimis@pinstudios.com>
2021-11-06 21:57:54 +0200

    Add a concept of LexoRank

--- Sources/LexoRank/LexoRank.swift
+++ Sources/LexoRank/LexoRank.swift
@@ -1,3 +1,19 @@
-class LexoRank {
+//
+// Created by Raimundas Sakalauskas on 2021-11-06.
+//
 
+import Foundation
+
+struct LexoRank {
+    let bucket: LexoBucket
+
+    init?(string: String, numberSystem: LexoNumberSystem = Base36NumberSystem.instance) {
+        let parts = string.split(separator: "|")
+
+        guard parts.count == 2, let bucket = LexoBucket.fromString(String(parts.first!)) else {
+            return nil
+        }
+
+        self.bucket = bucket
+    }
 }
